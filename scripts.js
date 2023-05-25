05a372a0bb1ffd45067efe0cd1df8b2dcf90b1a2
forrestmorrison <forrestmorrison@gmail.com>
2023-05-25 11:46:53 -0500

    new cards & IntersectionObserver

--- scripts.js
+++ scripts.js
@@ -1,13 +1,11 @@
+const cards = document.querySelectorAll(".card")
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
-        if(entry.isIntersecting) {
-            entry.target.classList.add("show");
-        } else {
-            entry.target.classList.remove("show");
-        }
+        entry.target.classList.toggle("show", entry.isIntersecting)
     })
 })
 
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
+cards.forEach((card) => {
+    observer.observe(card)
+})
